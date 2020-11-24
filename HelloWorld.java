12d0a4946add3dbdd7ebddc8555efc8eeb83f1ac
wxj <xinjuan28@163.com>
2020-11-24 20:42:28 +0800

    javadoc命令使用

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld{
+	/**
+		* @author WXJ
+		* @param args no
+		* @since 1.0
+		* @throws null
+	*/
+	public static void main(String[] args){
+		System.out.println("你好");
+	}
+}
